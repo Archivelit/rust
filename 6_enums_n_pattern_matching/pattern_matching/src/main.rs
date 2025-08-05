cec6aabd35517aae5da9f84c00afc10d991e1994
Archivelit <dalbaeb1488322666@gmail.com>
2025-08-05 21:27:32 +0200

    match keyword and pattern matching in rust

---
+++ 6_enums_n_pattern_matching/pattern_matching/src/main.rs
@@ -0,0 +1,36 @@
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
+
+fn print_state_information(state: UsState) {
+    println!("State quarter from {state:?}!");
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            print_state_information(state);
+            25
+        } 
+    }
+}
+
+fn main() {
+    println!("{}", value_in_cents(Coin::Quarter(UsState::Alaska)))
+}
